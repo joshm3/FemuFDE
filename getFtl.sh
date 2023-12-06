b1852062421e14d3d3645e092c1577419154822d
joshm3 <joshm3@vt.edu>
2023-12-06 15:59:59 -0500

    files for modifying femu

---
+++ getFtl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp ~/femu/hw/femu/bbssd/ftl* .
+sudo chmod +666 ./ftl*
---
+++ putFtl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp ./ftl* ~/femu/hw/femu/bbssd/
+sudo chmod +664 ~/femu/hw/femu/bbssd/ftl*
